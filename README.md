12e229074685d8636ec03605db3868d358613eed
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2018-12-08 19:27:40 +0100

    Print V4L2 device capabilities

---
+++ README.md
@@ -0,0 +1,8 @@
+# V4L2 Examples
+
+Examples that demonstrate the Video for Linux version 2 (V4L2) API.
+
+## Examples
+
+1. **[example-1](https://github.com/kmdouglass/v4l2-examples/tree/master/example-1)** Print video
+   device capabilities
---
+++ example-1/example-1.c
@@ -0,0 +1,64 @@
+/**
+ * Video for Linux version 2 (V4L2) example 1 - print device capabilities
+ *
+ * Based on
+ * - https://gist.github.com/Circuitsoft/1126411
+ * - https://jayrambhia.com/blog/capture-v4l2
+ *
+ * Kyle M. Douglass, 2018
+ * kyle.m.douglass@gmail.com
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/videodev2.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+static const char DEVICE[] = "/dev/video0";
+
+/**
+ * Prints the capabilities of the device.
+ */
+int print_capabilities(int fd) {
+  int ret;
+  struct v4l2_capability caps = {0};
+  
+  ret = ioctl(fd, VIDIOC_QUERYCAP, &caps);
+  if (ret == -1) {
+    perror("Querying device capabilities");
+    return errno;
+  }
+
+  printf("Driver Caps:\n"
+         "  Driver: \"%s\"\n"
+         "  Card: \"%s\"\n"
+         "  Bus: \"%s\"\n"
+         "  Version: %d.%d\n"
+         "  Capabilities: %08x\n",
+         caps.driver,
+         caps.card,
+         caps.bus_info,
+         (caps.version>>16)&&0xFF,
+         (caps.version>>24)&&0xFF,
+         caps.capabilities);
+  
+  return 0;
+}
+
+int main(void) {
+  int fd;
+
+  // Open the device file
+  fd = open(DEVICE, O_RDWR);
+  if (fd < 0) {
+    perror(DEVICE);
+    return errno;
+  }
+
+  print_capabilities(fd);
+  
+  close(fd);
+  return 0;
+}
